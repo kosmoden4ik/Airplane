751edcecbc6a66e2a39cfab5ea11029d0819b36c
kosmoden4ik <kosmoden4ik@gmail.com>
2017-10-14 17:45:05 +0300

    version1.1

---
+++ Airplane/Airplane/Class1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airplane
+{
+    class Airplane
+    {
+        public int Capacity { get; private set; }
+        public bool AutoPilotOn { get; set; }
+        public void airplaneTeroristWin()
+        {
+            while (Altitude != -1)
+            {
+                --Altitude;
+            }
+            Console.WriteLine("++++++Alah-akbar++++++");
+        }
+        /// <summary>
+        /// Fuel consuption. kg/km
+        /// </summary>
+        public float Consuption { get; private set; }
+
+        public int Altitude { get; private set; }
+
+
+        public static decimal TicketPrice { get; set; }
+        public static int MinAltitudeAuto { get; set; }
+        public static int MaxAltitudeAuto { get; set; }
+
+        private int _altitudeIncrement;
+
+        public Airplane(int capacity, float consuption, int altitudeIncrement)
+        {
+            Altitude = 0;
+            AutoPilotOn = false;
+            Capacity = capacity;
+            Consuption = consuption;
+            _altitudeIncrement = altitudeIncrement;
+        }
+
+        public int Climb(int increment)
+        {
+            if (!AutoPilotOn) return Altitude += increment;
+
+            if (Altitude + increment < MaxAltitudeAuto)
+            {
+                return Altitude += increment;
+            }
+
+            return Altitude = MaxAltitudeAuto;
+        }
+
+        public int Down(int increment)
+        {
+            if (AutoPilotOn)
+            {
+                if (Altitude - increment > MinAltitudeAuto)
+                {
+                    return Altitude -= increment;
+                }
+                if (Altitude < MinAltitudeAuto) return Altitude;
+                return Altitude = MinAltitudeAuto;
+            }
+
+            if (Altitude - increment < 0) return Altitude = 0;
+            return Altitude -= increment;
+        }
+
+        public void SetAltitude(int targetAlitude)
+        {
+            if (targetAlitude > Altitude && AutoPilotOn == false)
+            {
+                while (Altitude != targetAlitude)
+                {
+                    Climb(1);
+                }
+            }
+            if (targetAlitude < Altitude && AutoPilotOn == false)
+            {
+
+                while (Altitude != targetAlitude)
+                {
+                    Down(1);
+                }
+            }
+            if (targetAlitude < Altitude && AutoPilotOn == true)
+            {
+
+                while (Altitude != targetAlitude && MinAltitudeAuto < Altitude)
+                {
+                    Down(1);
+                }
+            }
+            if (targetAlitude > Altitude && AutoPilotOn == true)
+            {
+                while (Altitude != targetAlitude && MaxAltitudeAuto > Altitude)
+                {
+                    Climb(1);
+                }
+            }
+        }
+    }
+
+}
---
+++ Airplane/Airplane/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airplane
+{ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("App started");
+        Airplane.MaxAltitudeAuto = 10000;
+        Airplane.MinAltitudeAuto = 2000;
+
+        Airplane airplane = new Airplane(140, 2.26F, 100);
+        //
+        airplane.AutoPilotOn = true;
+        Console.WriteLine(airplane.Altitude);
+        airplane.SetAltitude(11000);
+        Console.WriteLine(airplane.Altitude);
+        airplane.SetAltitude(1000);
+        Console.WriteLine(airplane.Altitude);
+        airplane.AutoPilotOn = false;
+        airplane.SetAltitude(12000);
+        Console.WriteLine(airplane.Altitude);
+        airplane.SetAltitude(500);
+        Console.WriteLine(airplane.Altitude);
+        airplane.airplaneTeroristWin();
+        Console.WriteLine(airplane.Altitude);
+        Console.ReadLine();
+
+    }
+}
+}
